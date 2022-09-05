76e707b4a218e66a09a093d1f65e07eddb7b9094
Ja7Ca <jarotsetiawan2503@gmail.com>
2022-09-05 17:33:31 +0700

    add task 08_Javascript-Refreshment

---
+++ 08_Javascript Refreshment/praktikum/2.js
@@ -0,0 +1,8 @@
+// 2
+// a. Jelaskan kenapa baris 21,22,23 tidak dapat ditampilkan?
+// b. Jelaskan knapa deklarasi pada baris 26, dapat menyebabkan error?
+// c. Dengan melakukan komen pada baris ke 26, apakah baris 28 dapat dieksekusi? jelaskan
+
+//  2.a => dikarenakan hasil dari kondisi if pada baris 21 bernilai false, sehingga tidak menjalakan blok kodenya
+// 2.b => dikarenakan variable nama berupa const, sehingga tidak dapat dirubah valuenya
+// 2.c => tidak dapat, dikarena variable asal dideclar berada dalam blok function, sedangkan pemanggilan variable diluar blok, sehingga tidak dapat dipanbggil
---
+++ 08_Javascript Refreshment/praktikum/5.js
@@ -0,0 +1,2 @@
+let value = [1, 2, 3, 4, 5, 6];
+console.log(value.map(value => value*2));
