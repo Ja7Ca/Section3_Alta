6ac60a51bc65eb2f1608005ff87772f7ee1ae01a
Ja7Ca <jarotsetiawan2503@gmail.com>
2022-09-13 20:27:54 +0700

    add task section 11 11_Introduction React

--- 11_Introduction React/README.md
+++ 11_Introduction React/README.md
@@ -1,7 +1,7 @@
-<h1 align= "center"><b>Resume Section</b></h1>
+<h1 align= "center"><b>Resume Section 11</b></h1>
 
 <ul>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>React -> React adalah library javascript untuk membuat user interface (UI) yang interaktif dan cepat pada web maupun mobile</li>
+    <li>Kenapa React? -> 1. Declarative, 2. Berbasis Komponen, 3. Belajar Sekali, Menulis dimana saja</li>
+    <li>Syntax CLI -> dir/ls memunculkan directori, cd memasukki direktori, mkdir membuat direktori</li>
 </ul>
---
+++ 11_Introduction React/praktikum/test/src/Home.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import "./asset/css/main.css"
+import "./asset/css/bootstrap.min.css"
+import LogoAtas from "./asset/img/logo-ALTA@2x.png";
+import FotoProfile from "./asset/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg";
+
+const Home = () => {
+    return (
+        <div>
+          <header>
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-8 col-md-12 col-sm-12 col-12">
+                  <div className="alterra">
+                    <a href="/home">
+                        <img className="logo-atas" src={LogoAtas}/>
+                    </a>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                  {/* <div class="container"> */}
+                  <div className="row align-items-center navig">
+                    <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right"><a href="index.html" className="aktif">HOME</a></div>
+                    <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center"><a href="about.html">ABOUT</a></div>
+                    <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-left"><a href="#">EXPERIENCE</a></div>
+                    <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right"><a href="form.html">CONTACT</a></div> 
+                  </div>
+                  {/* </div> */}
+                </div>    
+              </div>
+            </div> 
+          </header>
+          <div className="container h-100">
+            <div className="row align-items-center isi-home">
+              <div className="col-lg-4 col-md-12 col-sm-12  col-12">
+                <img id="foto-profil" src={FotoProfile} />
+              </div>
+              <div className="col-lg-8 col-md-12 col-sm-12 col-12">
+                <div className="main-p">
+                  <p className="sapaan">Hi, my name is </p>
+                  <p className="nama">Anne Sullivan</p>
+                  <p className="hobi">I build things for the web</p>
+                  <p className="tombol"><a href="about.html">Get In Touch</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+// const Home = () => {
+//     return (
+//         <div>
+//       <header>
+//         <div className="container">
+//           <div className="row align-items-center">
+//             <div className="col-lg-8 col-md-12 col-sm-12 col-12">
+//               <div className="alterra">
+//                 <a href="/home">
+//                   <img className="logo-atas" src={LogoAtas} alt="Logo Atas" />
+//                 </a>
+//               </div>
+//             </div>
+//             <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+//               <div className="row align-items-center navig">
+//                 <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-end">
+//                   <a href="/home" className="aktif">
+//                     HOME
+//                   </a>
+//                 </div>
+//                 <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center">
+//                   <a href="#foo">ABOUT</a>
+//                 </div>
+//                 <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-start">
+//                   <a href="#foo">EXPERIENCE</a>
+//                 </div>
+//                 <div className="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-end">
+//                   <a href="#foo">CONTACT</a>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </header>
+//       <div className="container h-100">
+//         <div className="row align-items-center isi-home">
+//           <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+//             <img id="foto-profil" src={FotoProfile} alt="Foto Profile" />
+//           </div>
+//           <div className="col-lg-8 col-md-12 col-sm-12 col-12">
+//             <div className="main-p">
+//               <p className="sapaan">Hi, my name is</p>
+//               <p className="nama">Anne Sullivan</p>
+//               <p className="hobi">I build things for the web</p>
+//               <p className="tombol">
+//                 <a href="#foo">Get In Touch</a>
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default Home;
